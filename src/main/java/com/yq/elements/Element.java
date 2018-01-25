e8209866091b00f31296d40533283ebf41d116a6
rawqing <kingstar95d@gmail.com>
2018-01-25 23:08:11 +0800

    first

---
+++ src/main/java/com/yq/elements/Element.java
@@ -0,0 +1,11 @@
+package com.yq.elements;
+
+/**
+ * Created by king on 16/11/8.
+ */
+
+public interface Element<E>{
+
+    E way();
+
+}
---
+++ src/main/java/com/yq/elements/Finder.java
@@ -0,0 +1,12 @@
+package com.yq.elements;
+
+/**
+ * Created by king on 2018/1/25.
+ */
+public class Finder {
+
+    static public Element id(){
+
+        return null;
+    }
+}
